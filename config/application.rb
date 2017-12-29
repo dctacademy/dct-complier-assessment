c11ca81f3ffdd094ba16dd3c4971ec6e91e8af30
Pavan Kumar <kumarpavan1309@gmail.com>
2017-12-29 21:17:12 +0530

    submissions codemirror

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Cc
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('app', 'services')
+    config.time_zone = "Chennai"
   end
 
 end
