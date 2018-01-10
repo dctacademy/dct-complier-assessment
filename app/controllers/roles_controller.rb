f29a106bb1a9c267f1ef842e6ef3409e713c5d71
= <ani@dctacademy.com>
2018-01-10 16:46:51 +0530

    with exception notification

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,6 +1,8 @@
 class RolesController < ApplicationController
-  before_action :set_role, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  load_and_authorize_resource
 
+  before_action :set_role, only: [:show, :edit, :update, :destroy]
   # GET /roles
   # GET /roles.json
   def index
