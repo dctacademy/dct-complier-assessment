e6d0e078dccb6670936d98f13d94820aa2955c10
= <ani@dctacademy.com>
2018-01-13 14:18:58 +0530

    add notes to assignment groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180112053844) do
+ActiveRecord::Schema.define(version: 20180113083627) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180112053844) do
     t.datetime "updated_at",                  null: false
     t.datetime "due_datetime"
     t.boolean  "allow_points", default: true
+    t.text     "notes"
   end
 
   create_table "assignments", force: :cascade do |t|
