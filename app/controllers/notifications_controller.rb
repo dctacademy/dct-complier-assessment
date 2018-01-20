46fea7b4e22736eaf7f4ed80b1683d4c57daa241
= <ani@dctacademy.com>
2018-01-20 12:27:16 +0530

    with correct spelling

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
 
-	before_action :authenticate_user!, expect: [:index]
+	before_action :authenticate_user!, except: [:index]
 	load_and_authorize_resource
 	
 	def index
