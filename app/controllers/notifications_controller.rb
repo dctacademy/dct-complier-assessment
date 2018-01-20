9e9d8b057f7b92bd25ab3b1ff366635c02f0e8ea
= <ani@dctacademy.com>
2018-01-20 12:23:23 +0530

    api exposed

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
 
-	before_action :authenticate_user!
+	before_action :authenticate_user!, expect: [:index]
 	load_and_authorize_resource
 	
 	def index
